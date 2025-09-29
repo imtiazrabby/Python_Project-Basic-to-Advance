2eca5cf18091cf66db8c84c2a2633c09daa3f318
Md. Rabbi Ahammed <rabby4772@gmail.com>
2025-09-29 23:39:00 +0600

    Update Quiz_Game.py
    commit

--- Quiz_Game.py
+++ Quiz_Game.py
@@ -1,5 +1,12 @@
 print("Welcome to my Computer Quiz!!")
+
 playing = input ("Do you want to play?")
 
 if playing !="yes":
     quit()
+
+print("Okay! Let's play :)")
+
+answer = input ("What does CPU Stand for? ")
+if answer == "Central Processing Unit" :
+    print("Correct!")
