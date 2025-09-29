a7f1a527d716c252f9f4d98d3c3f6d5e225252c1
Md. Rabbi Ahammed <rabby4772@gmail.com>
2025-09-30 00:31:57 +0600

    Update Quiz_Game.py
    commit

--- Quiz_Game.py
+++ Quiz_Game.py
@@ -13,3 +13,45 @@ if answer == "Central Processing Unit" :
 else:
     print("Incorrect!")
 
+answer = input ("What does GPU Stand for? ")
+
+if answer == "Graphics Processing Unit" :
+    print ("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input ("What does RAM Stand for? ")
+
+if answer == "Random Access Memory" :
+    print ("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input ("What does ROM Stand for? ")
+
+if answer == "Read-Only Memory" :
+    print ("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input ("What does PSU Stand for? ")
+
+if answer == "Power Supply" :
+    print ("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input ("What does CSE Stand for? ")
+
+if answer == "Computer Science and Engineering" :
+    print ("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input ("What does ICT Stand for? ")
+
+if answer == "Information and Communication Technology" :
+    print ("Correct!")
+else:
+    print("Incorrect!")
+
