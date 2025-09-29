8c6c2897b5af49e8023a7824d4ed6d5de0a51d77
Md. Rabbi Ahammed <rabby4772@gmail.com>
2025-09-30 01:28:50 +0600

    Update Quiz_Game.py
    commit

--- Quiz_Game.py
+++ Quiz_Game.py
@@ -2,58 +2,83 @@ print("Welcome to my Computer  Quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing.lower() !="yes":
+if playing.lower() != "yes":
     quit()
 
 
 print("Okay! Let's Play :) ")
+score = 0
+
 answer = input ("What does CPU Stand for? ")
 
-if answer.lower() == "Central Processing Unit" :
+if answer.lower() == "central processing unit" :
     print ("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
 
 answer = input ("What does GPU Stand for? ")
 
-if answer.lower() == "Graphics Processing Unit" :
+if answer.lower() == "graphics processing unit" :
     print ("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+
+
 
 answer = input ("What does RAM Stand for? ")
 
-if answer.lower() == "Random Access Memory" :
+if answer.lower() == "random access memory" :
     print ("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+
+
 
 answer = input ("What does ROM Stand for? ")
 
-if answer.lower() == "Read-Only Memory" :
+if answer.lower() == "read only memory" :
     print ("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+
 
 answer = input ("What does PSU Stand for? ")
 
-if answer.lower() == "Power Supply" :
+if answer.lower() == "power supply" :
     print ("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+
 
 
 answer = input ("What does CSE Stand for? ")
 
-if answer.lower() == "Computer Science and Engineering" :
+if answer.lower() == "computer science and engineering" :
     print ("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+
+
 
 answer = input ("What does ICT Stand for? ")
 
-if answer.lower() == "Information and Communication Technology" :
+if answer.lower() == "information and communication technology" :
     print ("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
+
+print("You got " + str(score) + " question correct. ")  
+print("You got " + str((score/7)*100) + " %.")  
