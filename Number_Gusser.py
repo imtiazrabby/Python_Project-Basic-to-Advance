9b3ed03698455a775f7095c6426c155c069de8c2
Md. Rabbi Ahammed <rabby4772@gmail.com>
2025-09-30 09:31:12 +0600

    Update Number_Gusser.py
    commit

--- Number_Gusser.py
+++ Number_Gusser.py
@@ -12,13 +12,12 @@ else:
     print('Please type a number next time.')
     quit()
 
-random_number = random.randint(1, top_of_range)
+random_number = random.randint(0, top_of_range)
 guesses = 0
 
 while True:
     guesses += 1
     user_guess = input("Make a guess: ")
-
     if user_guess.isdigit():
         user_guess = int(user_guess)
     else:
@@ -26,7 +25,7 @@ while True:
         continue
 
     if user_guess == random_number:
-        print("🎉 You got it!")
+        print("You got it!")
         break
     elif user_guess > random_number:
         print("You were above the number!")
