ae1bf958c8dc3c7e2f297319fd96a756a7f144df
Md. Rabbi Ahammed <rabby4772@gmail.com>
2025-09-30 12:03:45 +0600

    Update Rock_Paper_Scissors.py
    commit

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -3,39 +3,34 @@ import random
 user_wins = 0
 computer_wins = 0
 
-options = ["Rock", "Paper", "Scissors"]
+options = ["rock", "paper", "scissors"]  # lowercase for consistency
 
-while true:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit.").lower()
-    if user_input =="q":
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    
+    if user_input == "q":
         break
 
-    if user_input in options:
+    if user_input not in options:
+        print("Invalid input. Try again!")
         continue
-    random_number = random.randint(0,2)
-    #rock:0, paper:1, scissors:2
+
+    random_number = random.randint(0, 2)
     computer_pick = options[random_number]
-    print("Computer picked ", computer_pick + ".")
-    
-    if user_input == "rock" and" scissors":
-        print("You won!")
-        user_wins +=1
-        continue
+    print("Computer picked", computer_pick + ".")
 
-    elif user_input == "paper" and" rock":
-        print("You won!")
-        user_wins +=1
-        continue
+    if user_input == computer_pick:
+        print("It's a tie!")
 
-    elif user_input == "scissors" and" paper":
+    elif (user_input == "rock" and computer_pick == "scissors") or \
+         (user_input == "paper" and computer_pick == "rock") or \
+         (user_input == "scissors" and computer_pick == "paper"):
         print("You won!")
-        user_wins +=1
-        continue
+        user_wins += 1
     else:
         print("You lost!")
-        computer_wins +=1
+        computer_wins += 1
 
-print("You won", user_wins,"times.")
-print("The computer won", computer_wins,"times.")
-
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
 print("Goodbye!")
